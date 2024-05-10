40b56ec665f91b0d57a67ad47e34cb126a3b56ff
Arpit garg <arpitgarg5689@gmail.com>
2024-05-11 00:20:31 +0530

    update homepge

---
+++ script.js
@@ -0,0 +1,26 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+let counter = 1;
+setInterval(function () {
+  let slideBtn = document.getElementById("radio" + counter);
+  //   Checks the checkbox
+  slideBtn.checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 2000);
